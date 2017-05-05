801de1c10e88d3d2cb28a277cb729a8d82542259
Nikko Ortega <NFCOrtega89@gmail.com>
2017-05-04 22:26:11 -0700

    footer

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className="row">
+        <a href="https://darksky.net/poweredby/">Powered By DarkSky</a>
+      </div>
+    )
+  }
+}
