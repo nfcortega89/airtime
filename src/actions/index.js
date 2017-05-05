ed5cd117650d92ad3d4bb9496c5e618df6e1eb9a
Nikko Ortega <NFCOrtega89@gmail.com>
2017-05-04 22:24:20 -0700

    added server

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
 import { areas } from '../../coordinates';
-const API_KEY = '4195135e9c7034878d78c6f4d7f1f88d';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
+const ROOT_URL = `https://vast-cliffs-24756.herokuapp.com`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather() {
   return (dispatch) => {
     const promises = areas.map(({lat, lon, name}) => {
-      const url = `${ROOT_URL}&lat=${lat}&lon=${lon}&units=imperial`;
+      const url = `${ROOT_URL}?lat=${lat}&lon=${lon}`;
       return axios.get(url);
     })
 
