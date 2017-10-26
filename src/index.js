fe2a91e86bef28ba09128288bff92d6632297d2f
Niccolo Ortega <nfcortega89@gmail.com>
2017-10-25 17:06:40 -0700

    adding loader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import ReduxThunk from 'redux-thunk'
-import { createStore, applyMiddleware } from 'redux';
-import { Router, browserHistory } from 'react-router';
-import routes from './routes';
-import reducers from './reducers';
+import React from "react";
+import ReactDOM from "react-router-dom";
+import { Provider } from "react-redux";
+import ReduxThunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+import { Router, browserHistory } from "react-router";
+import routes from "./routes";
+import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware(ReduxThunk)(createStore);
 window.store = createStoreWithMiddleware(reducers);
@@ -14,5 +14,6 @@ const store = createStoreWithMiddleware(reducers);
 ReactDOM.render(
   <Provider store={store}>
     <Router history={browserHistory} routes={routes} />
-  </Provider>
-  , document.querySelector('.container-fluid'));
+  </Provider>,
+  document.querySelector(".container-fluid")
+);
